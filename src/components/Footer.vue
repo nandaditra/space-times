a2734c322f0251af270b8e5eb9ea97ed3ceeba57
nandaditra <nandaditra56@gmail.com>
2023-04-09 16:56:03 +0700

    add response code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,13 +8,13 @@
    <footer>
        <div class="container">
            <div class="row">
-                <div class="col-6 mr-4">
+                <div class="col-sm-12 col-md-6 mr-4">
                     <img class="logo" v-bind:src="'/src/assets/img/logo.png'" alt="logo.png">
                     <p class="p-footer mt-4">The images.nasa.gov API is organized around REST, has predictable/resource­-oriented URLs and uses HTTP response codes to indicate API errors. This API uses built-­in HTTP features such as HTTP authentication and HTTP verbs, which are understood by many off-­the-­shelf HTTP clients. Please note that JSON is returned by all API responses, including errors. Each of the endpoints described</p>
                 </div>
-                <div class="col-6 mr-0">
+                <div class="col-sm-12 col-md-6 mr-0">
                     <div class="row">
-                         <div class="col-4">
+                         <div class="col-sm-8 col-md-4">
                              <p class="text-center"><b>Media Social</b></p>
                              <ul>
                                 <li>Facebook</li>
@@ -23,7 +23,7 @@
                                 <li>Tiktok</li>
                              </ul>
                          </div>
-                         <div class="col-4 ">
+                         <div class="col-sm-8 col-md-4">
                             <p class="text-center"><b>Other</b></p>
                             <ul>
                                 <li>About Us</li>
@@ -32,7 +32,7 @@
                                 <li>Safety</li>
                             </ul>
                          </div>
-                         <div class="col-4">
+                         <div class="col-sm-8 col-md-4">
                             <p class="text-center"><b>Sponsored By</b></p>
                             <img class="w-100" v-bind:src="'/src/assets/img/nasa.png'" alt="nasa.png">
                          </div>
